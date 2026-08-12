f71f91887474395808daa6bb1c42081b381dd6d0
mehran-zahid <mehranzahid13@gmail.com>
2026-08-12 08:37:26 +0500

    Add bright green console banner and X-Tubewell-Build-Sync response header v1.0.8

--- api/fetch_mepco.js
+++ api/fetch_mepco.js
@@ -2,6 +2,7 @@ export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('X-Tubewell-Build-Sync', '2026-08-12-v1.0.8');
 
   if (req.method === 'OPTIONS') {
     return res.status(200).end();
