33b6f297a8a978568f5975b298a1569574e4f415
Saim Sajid <saimsajid0341@gmail.com>
2026-08-14 23:32:46 +0500

    fix: use ScraperAPI premium residential proxies and country_code pk to bypass PITC firewall block

--- api/fetch_mepco.js
+++ api/fetch_mepco.js
@@ -61,7 +61,8 @@ export default async function handler(req, res) {
   const tryScraperAPI = async () => {
     const targetUrl = 'http://bill.pitc.com.pk/mepcobill';
     // premium=true forces Residential IPs which bypasses PITC's datacenter firewall!
-    const scraperUrl = `http://api.scraperapi.com?api_key=${SCRAPER_API_KEY}&url=${encodeURIComponent(targetUrl)}&session_number=${cleanRef}&keep_headers=true&premium=true`;
+    // country_code=pk ensures it uses Pakistani IPs which PITC trusts!
+    const scraperUrl = `http://api.scraperapi.com?api_key=${SCRAPER_API_KEY}&url=${encodeURIComponent(targetUrl)}&session_number=${cleanRef}&keep_headers=true&premium=true&country_code=pk`;
 
     // Step 1: GET to retrieve hidden tokens
     const res1 = await fetch(scraperUrl, {
